6cf1626d44f2a54d3b38904f046e6ddf8dbbd2c7
sai <sai@gmail.com>
2025-09-09 19:57:10 +0530

    commiting

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -5,3 +5,9 @@ echo "Please enter your username:"
 read USERNAME
 
 echo "Entered username is: $USERNAME"
+
+echo "Please enter your Password:"
+
+read PASSWORD
+
+echo "given password is $PASSWORD"
