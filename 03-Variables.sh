c0f21d2728d743288d452137c6185ce955cdcd67
sai <sai@gmail.com>
2025-09-09 19:49:55 +0530

    commiting

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hi $PERSON1, how are you?"
+echo "$PERSON1: I am fine $PERSON2, what about you?"
+echo "$PERSON2: ok $PERSON1 great to hear"
