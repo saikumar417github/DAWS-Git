6f43e39a885af5ef472985fbbc65262b15aef665
sai <sai@gmail.com>
2025-09-09 20:01:43 +0530

    commiting

---
+++ 06-Array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("APPLE" "BANANA" "GRAPE")
+
+echo "First fruit is ${FRUITS[0]}"
+echo "Second fruit is ${FRUITS[1]}"
+echo "Third fruit is ${FRUITS[3]}"
+
+echo "Fruits are ${FRUITS[@]}"
