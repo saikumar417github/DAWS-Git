358b61ba9b1ae2ccb9b876e1b6e04175f90173e7
sai <sai@gmail.com>
2025-09-10 21:31:55 +0530

    commiting

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -4,3 +4,8 @@ echo "All variables passed to the script: $@"
 echo "Number of variables passwed to script: $#"
 echo "Script name is: $0"
 echo "Current working directory $PWD"
+echo "Home directory of the user :$HOME"
+echo "PID of the script: $$"
+sleep 100 &&
+
+echo "PID of the last background command: $!"
